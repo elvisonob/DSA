8f72b31dd7038f5f9af65845250a1c348d5cb94f
elvisonob <elvisonob@yahoo.com>
2026-03-09 18:52:49 +0000

    DSA compute

--- closure.js
+++ closure.js
@@ -2,10 +2,10 @@ const testArray = {
   2: 'Elvis',
   3: 'Ese',
   4: 'Eric',
-  5: 'Ehiment',
+  5: 'Ehimen',
   length: 3,
 };
 
 const result = Array.from(testArray);
 
-console.log(results);
+console.log(result);
