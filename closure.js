804addc7b86990e0d8289e43849bac2748854e30
elvisonob <elvisonob@yahoo.com>
2026-05-15 05:47:35 +0100

    set, get, has, flatMap refresher

--- closure.js
+++ closure.js
@@ -1,13 +1,26 @@
-const cars = [
-  ['Benz', 'Ford'],
-  ['Renault', 'Sienna', 'Peugeout', 'Avalon'],
-  ['Mazda', 'Volkswagens', 'Nissan'],
-];
+let nums = [1, 2, 3, 4, 5, 6];
 
-const updatedBoard = cars;
+let result = nums
+  .filter((n) => n % 2 === 0)
+  .map((n) => n * 2)
+  .reduce((a, b) => a + b);
 
-console.log(
-  ...updatedBoard.map((innerArray) => {
-    return [...innerArray];
-  }),
-);
+let zoo = new Map();
+
+zoo.set('Zebra', 5);
+zoo.set('Orange', 4);
+zoo.set('Mango', 6);
+zoo.set('Apple', 10);
+
+console.log(zoo.get('Apple'));
+
+for (let [key, value] of zoo) {
+  console.log(key + ':' + value);
+}
+
+const userScores = new Map();
+
+userScores.set('alice', 100);
+userScores.set('bob', 75);
+
+console.log(userScores);
