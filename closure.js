00ca70575fb0223e45aef67709d153847dc2c9a4
elvisonob <elvisonob@yahoo.com>
2026-05-18 05:17:31 +0100

    do while loop

--- closure.js
+++ closure.js
@@ -1,26 +1,13 @@
-let nums = [1, 2, 3, 4, 5, 6];
+let n = 5;
 
-let result = nums
-  .filter((n) => n % 2 === 0)
-  .map((n) => n * 2)
-  .reduce((a, b) => a + b);
-
-let zoo = new Map();
-
-zoo.set('Zebra', 5);
-zoo.set('Orange', 4);
-zoo.set('Mango', 6);
-zoo.set('Apple', 10);
-
-console.log(zoo.get('Apple'));
-
-for (let [key, value] of zoo) {
-  console.log(key + ':' + value);
+while (n < 5) {
+  n++;
+  console.log(n);
 }
 
-const userScores = new Map();
+let i = 5;
 
-userScores.set('alice', 100);
-userScores.set('bob', 75);
-
-console.log(userScores);
+do {
+  i++;
+  console.log(i);
+} while (i < 5);
